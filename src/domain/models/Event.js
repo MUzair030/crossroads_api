65e00766a52471475cda751d9bb16694f6f0d8e7
moosa <moosa.sattar6111@gmail.com>
2025-05-16 01:37:08 +0500

    testing Create Event

--- src/domain/models/Event.js
+++ src/domain/models/Event.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import stagePostSchema from "./StagePostSchema.js"; // adjust the path as needed
+import stagePostSchema from "./StagePost.js"; // adjust the path as needed
 
 
 const ticketSchema = new mongoose.Schema({
