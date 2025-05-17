4edad5bb30f131bd1486764db9cec4d0fdca3ba7
moosa <moosa.sattar6111@gmail.com>
2025-05-18 03:42:17 +0500

    Testing Notifications

--- src/application/services/NotificationService.js
+++ src/application/services/NotificationService.js
@@ -12,7 +12,7 @@ import User from '../../domain/models/User.js';
  * @param {ObjectId} params.receiverId - receiver user ID
  * @param {ObjectId} [params.senderId] - optional sender
  * @param {Object} [params.metadata] - optional extra info (e.g., eventId, serviceId)
- */import admin from 'firebase-admin';
+*/const admin = require('firebase-admin');
 
 export const registerNotification = async ({
   type,
