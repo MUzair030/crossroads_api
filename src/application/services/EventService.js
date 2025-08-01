823ecf7a4f3ad555b9b4f4a9fbe71dfe6d3386d8
moosa <moosa.sattar6111@gmail.com>
2025-08-02 04:05:37 +0500

    Fixing Bugs

--- src/application/services/EventService.js
+++ src/application/services/EventService.js
@@ -53,7 +53,7 @@ async createEvent(data) {
 
   group.eventIds.push(event._id);
   group.eventStatuses.push({
-    eventId: event._id,
+    eventId:  new mongoose.Types.ObjectId(event._id),
     status: data.isLive ? 'live' : 'upcoming',
   });
 
