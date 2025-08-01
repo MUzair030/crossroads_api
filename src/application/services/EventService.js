6f740d53b4be492f2816da3f1def12f308e0aee9
moosa <moosa.sattar6111@gmail.com>
2025-08-02 03:46:42 +0500

    Fixing Bugs

--- src/application/services/EventService.js
+++ src/application/services/EventService.js
@@ -49,8 +49,7 @@ async createEvent(data) {
   if (groupId) {
     const group = await GroupRepository.findById(groupId);
     group.eventIds.push(event._id);
-    group.eventStatuses=e
-    group.eventStatuses || [];
+    group.eventStatuses = group.eventStatuses || [];
     group.eventStatuses.push({
   eventId: event._id,
   status: data.isLive ? 'live' : 'upcoming',
