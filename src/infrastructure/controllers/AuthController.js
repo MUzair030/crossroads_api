8d1e1081c97cce56e18bec64e248f230145244d7
moosa <moosa.sattar6111@gmail.com>
2025-08-08 19:02:00 +0500

    OAuth fix

--- src/infrastructure/controllers/AuthController.js
+++ src/infrastructure/controllers/AuthController.js
@@ -121,7 +121,7 @@ router.post('/google/mobile', async (req, res) => {
     if (!user) {
       const verificationToken = Math.floor(10000 + Math.random() * 90000).toString();
       
-          const userData = {googleId,password: null ,name, email, userName, verificationToken };
+          const userData = {googleId,password: null ,name, email, verificationToken };
            user = await this.userRepository.save(userData);
           await emailService.sendVerificationEmail(savedUser, verificationToken);
      
