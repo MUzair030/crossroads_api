5a31e89a45a2d99c5cf8a6c75230e634771b690b
moosa <moosa.sattar6111@gmail.com>
2025-08-08 19:22:08 +0500

    OAuth fix

--- src/application/services/AuthService.js
+++ src/application/services/AuthService.js
@@ -203,7 +203,7 @@ async refreshAccessToken(refreshToken) {
   if (!user) {
     const verificationToken = Math.floor(10000 + Math.random() * 90000).toString();
     const userData = { googleId, password: null, name, email, verificationToken };
-    user = await userRepository.save(userData);
+    user = await this.userRepository.save(userData);
     await emailService.sendVerificationEmail(user, verificationToken);
   }
 
