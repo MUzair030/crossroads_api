21dde4a3038f1c14aebf924c1c46a55bfa5b7346
moosa <moosa.sattar6111@gmail.com>
2025-08-27 23:07:39 +0500

    Fixes

--- src/infrastructure/repositories/UserRepositoryImpl.js
+++ src/infrastructure/repositories/UserRepositoryImpl.js
@@ -102,7 +102,7 @@ class UserRepositoryImpl extends UserRepository {
 
   
   async updateProfilePicture(userId, file) {
-    const user = await this.getUserById(userId);
+    const user = await this.findById(userId);
     if (user?.profilePicture) {
       await FileUploadService.deleteFromS3(user.profilePicture);
     }
